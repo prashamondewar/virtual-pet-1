bc9b015fb2b619071f1672b2b5cb4bba9c62bf87
prashamondewar <71713748+prashamondewar@users.noreply.github.com>
2021-05-24 05:31:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,18 @@
 var database;
-var food;
-var foodStock;
+var foodS;
+var foodStock; 
 var dog;
-var happyDog;
-
-function preload()
-{
-  doggo = loadImage("images/dogImg.png");
-  doghappy = loadImage("images/dogImg1.png")
-}
+var happyDog; 
+function preload() { 
+doggo = loadImage("images/dogImg.png"); 
+doghappy = loadImage("images/dogImg1.png") }
 
 function setup() {
 	createCanvas(500, 500);
   dog = createSprite(200,120,40,60)
   dog.addImage(doggo)
-  database = firebase;
-  foodStock = database.ref('food')
+  database = firebase.database;
+  foodStock = database.ref("food")
   foodStock.on("value", readStock)
 }
 
@@ -24,24 +21,25 @@ function draw() {
 background(46,139,87)
 if (keyDown(UP_ARROW)) {
   foodStock = foodStock-1;
-  writeStock(food);
+  writeStock(foodS);
   dog.addImage(doghappy);
   
 }
 
   drawSprites();
-  Text = "foodStock" + "=" + foodStock, 100, 150;
-  textSize = 4;
+  Text = "foodStock" + "=" + foodS, 200, 150;
+  textSize = 14;
   fill('black')
 }
 function readStock(data){
-  food = data.val();
+  foodS = data.val();
 }
 function writeStock(x){
- database.ref('/').update({
-   food:x
- })
+  if(x<=0){ x=0; }
+  else{ x=x-1; } 
+  database.ref('/').update({ food:x }) 
 }
+ 
 
 
 
